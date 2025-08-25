0c2bb43b95b57acd07badbb9c6e907fc0bcfec7b
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2025-08-25 13:51:32 +0700

    add: contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,9 +9,15 @@ const Contact = () => {
       <motion.div variants={fadeTop} {...motionStep}>
         <section id="contact" className="flex justify-center">
           <div className="container py-12 max-w-screen-xl px-4 bg-[#FF8500] group rounded-xl">
-            <div>
-              <h1>Now, It`s your Turn</h1>
-              <p></p>     
+            <div className="flex flex-col text-center justify-center gap-y-12">
+              <h1 className="text-white lg:text-6xl">Wanna Talk With Us?</h1>
+              <p className="text-white lg:text-xl font-light">
+                Don`t hesitate to get in touch. <br/> Take the first step by clicking
+                the button below.
+              </p>
+              <Link href="mailto:miraiistudio.team@gmail.com">
+                <button className="bg-white text-orange-500 text-xl py-4 px-6 rounded-full font-medium hover:bg-orange-100 transition transform duration-300">Get in Touch</button>
+              </Link>{" "}
             </div>
           </div>
         </section>
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,44 +5,17 @@ import { LiaInstagram, LiaLinkedin } from "react-icons/lia";
 
 const Footer = () => {
   return (
-    <footer className="divide-y bg-[#090A0C]">
-      <div className="container max-w-screen-xl py-10  mx-auto space-y-0 lg:flex-row lg:space-y-0">
-        <div className="w-full border"></div>
-        <div className="flex flex-col lg:flex-row justify-start px-3">
-          <div className="w-full lg:w-2/3">
-            <Image src="/image/MIRAI.webp" alt="" width={200} height={100} />
-            <div className="flex flex-wrap lg:flex-nowrap lg:flex-row justify-start gap-3 lg:gap-0 lg:space-x-5 w-full lg:w-fit">
-              <Link href="/#services">
-                <button className=" w-fit hover:bg-white bg-[#090A0C] hover:text-black text-white border transition transform duration-300 rounded-full px-6 py-2 text-xs lg:text-sm">
-                  Education Game
-                </button>
-              </Link>
-              <Link href="/#services">
-                <button className=" w-fit hover:bg-white bg-[#090A0C] hover:text-black text-white border transition transform duration-300 rounded-full px-6 py-2 text-xs lg:text-sm">
-                  Gamification
-                </button>
-              </Link>
-              <Link href="/#services">
-                <button className=" w-fit hover:bg-white bg-[#090A0C] hover:text-black text-white border transition transform duration-300 rounded-full px-6 py-2 text-xs lg:text-sm">
-                  Board Game
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="w-full lg:w-1/3 flex flex-col lg:justify-end py-8 lg:py-0">
-            <div className="flex lg:justify-end">
-              <Link target="_blank" href="https://www.instagram.com/miraii.studio/">
-                <LiaInstagram size={48} className="w-8 lg:w-fit lg:hover:text-gray-300 transition transform duration-300" />
-              </Link>
-              <Link target="_blank" href="https://www.linkedin.com/company/miraii-studio/">
-                <LiaLinkedin size={48} className="w-8 lg:w-fit lg:hover:text-gray-300 transition transform duration-300" />
-              </Link>
-            </div>
-            <div className="lg:py-3 text-sm lg:text-base text-left lg:text-end dark:text-[#C7C7C7]">
-              © 2025 Miraii Studio. All rights reserved.
-            </div>
-          </div>
-        </div>
+    <footer className="px-4 w-screen flex justify-center">
+      <div className="py-6 text-base text-center text-[#141414] flex flex-row items-center gap-2 font-medium">
+        © 2025 
+        <Link
+          href="https://miraiistudio.com/"
+          className="hover:opacity-80 transition-all duration-300 font-semibold text-orange-500 hover:text-orange-400"
+          target="_blank"
+        >
+          Miraii Studio.
+        </Link>{" "}
+         All rights reserved.
       </div>
     </footer>
   );
